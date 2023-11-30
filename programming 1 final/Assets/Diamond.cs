9f78e9b9bb7da41ad944cdb953ff1ed08f07c8fd
jannromero <janndenzellromero@gmail.com>
2023-11-30 13:39:47 -0500

    added destructible diamonds

---
+++ programming 1 final/Assets/Diamond.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Diamond : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other){ //box collider isTrigger 
+        if(other.transform.tag == "Ball"){
+            Destroy(gameObject); 
+        }
+    }
+}
